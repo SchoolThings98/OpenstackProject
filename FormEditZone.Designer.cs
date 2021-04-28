6e1b2891d1ea30c86f08fe024b43aebd714429a0
Victor Boarato <64256349+ZiZiC4@users.noreply.github.com>
2021-04-28 17:34:32 +0100

    DNS WORKING

---
+++ FormEditZone.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace LTIOpenstackProject
+{
+    partial class FormEditZone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBoxDesc = new System.Windows.Forms.RichTextBox();
+            this.textBoxTTL = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TTL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descrição";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(156, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBoxDesc
+            // 
+            this.richTextBoxDesc.Location = new System.Drawing.Point(109, 180);
+            this.richTextBoxDesc.Name = "richTextBoxDesc";
+            this.richTextBoxDesc.Size = new System.Drawing.Size(213, 96);
+            this.richTextBoxDesc.TabIndex = 4;
+            this.richTextBoxDesc.Text = "";
+            // 
+            // textBoxTTL
+            // 
+            this.textBoxTTL.Location = new System.Drawing.Point(109, 108);
+            this.textBoxTTL.Name = "textBoxTTL";
+            this.textBoxTTL.Size = new System.Drawing.Size(213, 20);
+            this.textBoxTTL.TabIndex = 5;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(109, 39);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(213, 20);
+            this.textBoxEmail.TabIndex = 6;
+            // 
+            // FormEditZone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 386);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxTTL);
+            this.Controls.Add(this.richTextBoxDesc);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormEditZone";
+            this.Text = "FormEditZone";
+            this.Load += new System.EventHandler(this.FormEditZone_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBoxDesc;
+        private System.Windows.Forms.TextBox textBoxTTL;
+        private System.Windows.Forms.TextBox textBoxEmail;
+    }
+}
---
+++ FormEditZone.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTIOpenstackProject
+{
+    public partial class FormEditZone : Form
+    {
+        public string serverIP = "";
+        public string scopeToken = "";
+        public string zoneID = "";
+        public FormEditZone(string ip, string token, string id)
+        {
+            InitializeComponent();
+            serverIP = ip;
+            scopeToken = token;
+            zoneID = id;
+        }
+
+        private void FormEditZone_Load(object sender, EventArgs e)
+        {
+            OpenstackAPI openstack = new OpenstackAPI();
+
+            var response = openstack.getZone(serverIP, scopeToken, zoneID);
+            JObject zone = JObject.Parse(response.Content);
+            textBoxEmail.Text = (string)zone.SelectToken("email");
+            textBoxTTL.Text = (string)zone.SelectToken("ttl");
+            richTextBoxDesc.Text = (string)zone.SelectToken("description");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenstackAPI openstack = new OpenstackAPI();
+            openstack.editZone(serverIP, scopeToken, zoneID, textBoxEmail.Text, textBoxTTL.Text, richTextBoxDesc.Text);
+        }
+    }
+}
